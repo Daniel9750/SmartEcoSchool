3a6075f5a679ed1c7b0cff01518503be1867c036
Cristo RPS <cristobits0101@gmail.com>
2024-02-17 19:18:43 +0000

    añadiendo los modelos al controlador

--- app/Http/Controllers/SensorMeasurementController.php
+++ app/Http/Controllers/SensorMeasurementController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\SensorMeasurement;
+// Importing the models.
+use App\Models\Building;
+use App\Models\Measurement;
+use App\Models\Sensor;
+use App\Models\SensorType;
 
 class SensorMeasurementController extends Controller
 {
@@ -10,7 +14,7 @@ class SensorMeasurementController extends Controller
     public function bar()
     {
         // Get all sensor measurements.
-        $sensorMeasurements = SensorMeasurement::all();
+        // $sensorMeasurements = SensorMeasurement::all();
 
         // Returns the home page and passes the measurement data as the sensorMeasurements variable.
         return view('pages.graphics.table', compact('sensorMeasurements'));
@@ -19,7 +23,7 @@ class SensorMeasurementController extends Controller
     public function line()
     {
         // Get all sensor measurements.
-        $sensorMeasurements = SensorMeasurement::all();
+        // $sensorMeasurements = SensorMeasurement::all();
 
         // Returns the home page and passes the measurement data as the sensorMeasurements variable.
         return view('pages.graphics.table', compact('sensorMeasurements'));
@@ -29,7 +33,7 @@ class SensorMeasurementController extends Controller
     public function b()
     {
         // Get all sensor measurements.
-        $sensorMeasurements = SensorMeasurement::all();
+        // $sensorMeasurements = SensorMeasurement::all();
 
         // Returns the home page and passes the measurement data as the sensorMeasurements variable.
         return view('pages.graphics.table', compact('sensorMeasurements'));
@@ -38,7 +42,7 @@ class SensorMeasurementController extends Controller
     public function l()
     {
         // Get all sensor measurements.
-        $sensorMeasurements = SensorMeasurement::all();
+        // $sensorMeasurements = SensorMeasurement::all();
 
         // Returns the home page and passes the measurement data as the sensorMeasurements variable.
         return view('pages.graphics.table', compact('sensorMeasurements'));

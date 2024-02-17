9845a0cc538ce6e2fdd0a189807ecef00be336ad
Cristo RPS <cristobits0101@gmail.com>
2024-02-17 19:13:03 +0000

    Comentando en inglés

--- app/Http/Controllers/SensorMeasurementController.php
+++ app/Http/Controllers/SensorMeasurementController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\SensorMeasurement;
 
 class SensorMeasurementController extends Controller
 {
-    //
+    // Monthly charts.
+    public function bar()
+    {
+        // Get all sensor measurements.
+        $sensorMeasurements = SensorMeasurement::all();
+
+        // Returns the home page and passes the measurement data as the sensorMeasurements variable.
+        return view('pages.graphics.table', compact('sensorMeasurements'));
+    }
+
+    public function line()
+    {
+        // Get all sensor measurements.
+        $sensorMeasurements = SensorMeasurement::all();
+
+        // Returns the home page and passes the measurement data as the sensorMeasurements variable.
+        return view('pages.graphics.table', compact('sensorMeasurements'));
+    }
+
+    // Annual graphs.
+    public function b()
+    {
+        // Get all sensor measurements.
+        $sensorMeasurements = SensorMeasurement::all();
+
+        // Returns the home page and passes the measurement data as the sensorMeasurements variable.
+        return view('pages.graphics.table', compact('sensorMeasurements'));
+    }
+
+    public function l()
+    {
+        // Get all sensor measurements.
+        $sensorMeasurements = SensorMeasurement::all();
+
+        // Returns the home page and passes the measurement data as the sensorMeasurements variable.
+        return view('pages.graphics.table', compact('sensorMeasurements'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,10 @@ use Illuminate\Support\Facades\Route;
 // Import the graphics driver so that streams can execute data serialization functions.
 use App\Http\Controllers\SensorMeasurementController;
 
-// Graph views.
-Route::get('/graphics/line', [SensorMeasurementController::class,'line' ])->name('graphics.bar');
+// Monthly charts.
 Route::get('/graphics/bar' , [SensorMeasurementController::class,'bar'  ])->name('graphics.bar');
+Route::get('/graphics/line', [SensorMeasurementController::class,'line' ])->name('graphics.bar');
+
+// Annual graphs.
 Route::get('/graphics/bar' , [SensorMeasurementController::class,'bar'  ])->name('graphics.bar');
 Route::get('/graphics/bar' , [SensorMeasurementController::class,'bar'  ])->name('graphics.bar');
